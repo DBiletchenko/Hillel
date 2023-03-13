de39370a7564f3676b752f1cab6c04cb3b713440
DBiletchenko <denis.biletchenko@gmail.com>
2023-03-13 16:51:10 +0200

    Final code

--- Лекція 1. GIT/Task1/Program.cs
+++ Лекція 1. GIT/Task1/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int sum = 0;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                sum = sum + i;
+            }
+            Console.WriteLine(sum);
         }
     }
 }
