e8e8b85939bcf60a0511c02b939230bf91171705
DBiletchenko <denis.biletchenko@gmail.com>
2023-03-22 19:46:57 +0200

    Merged branches

---
+++ Lection 2/Task_2/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class Car : Vehicle
+    {
+        public int Mileage;
+
+        public override void Move()
+        {
+            Console.WriteLine("You've rode a car");
+        }
+        public override void Horn()
+        {
+            Console.WriteLine("Honk-honk");
+        }
+
+        public Car()
+        {
+            Console.WriteLine("You have bought a car");
+        }
+        public Car(int wheels, string manufacturer, string serialNum) : base(wheels, manufacturer, serialNum)
+        {
+
+        }
+    }
+}
---
+++ Lection 2/Task_2/Vehicle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal abstract class Vehicle
+    {
+        public int NumberOfWheels;
+        private int _vehicleValue;
+        private string _Manufacturer;
+        private string _serialNumber;
+        public abstract void Move();
+        public virtual void Horn()
+        {
+            Console.WriteLine("Beep-beep");
+        }
+
+
+        internal string manufacturer
+        {
+            get
+            {
+                return _Manufacturer;
+            }
+        }
+        internal string Serial
+        {
+            get
+            {
+                return _serialNumber;
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                return _vehicleValue;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Value of the vehicle can't be less or equal to 0");
+                }
+                else
+                {
+                    _vehicleValue = value;
+                }
+            }
+        }
+
+
+        public Vehicle()
+        {
+
+        }
+        public Vehicle(int wheels, string manufacturer, string serialNum)
+        {
+            NumberOfWheels = wheels;
+            _Manufacturer = manufacturer;
+            _serialNumber = serialNum;
+        }
+    }
+}
+
+
